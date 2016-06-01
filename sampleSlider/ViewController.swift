3d144b162391a18db069e89049899b8e4f0f26ac
TakayukiMorijiri <takayuki.m1206@gmail.com>
2016-06-01 12:43:53 +0900

    sliderの練習

--- sampleSlider/ViewController.swift
+++ sampleSlider/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+        
+        print(sender.value)
+        
+        //TODO:音量が0.5以上は「うるさいです」0.5以下なら「静かです」
+        if sender.value >= 0.5{
+            print("うるさいです！！")
+        }else{
+            print("しずかです")
+        }
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
